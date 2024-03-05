6b5d544e903ae4ea2ab02ecce30266a5967da76f
alejo saez <alejosaezgebicki@gmail.com>
2024-03-05 10:57:46 -0300

    modificacion de banner herder

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -7,14 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
         article.href = '#';
         article.className = 'group relative block bg-black max-w-xs';
         article.style.margin = '0 auto'; 
-        article.style.width = '300px'; 
-        article.style.height = '500px'; 
+        article.style.width = '200px'; 
+        article.style.height = '400px'; 
+      
 
         article.innerHTML = `
           <img alt="" src="${movie.poster}" class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50">
           <div class="relative p-4 sm:p-6 lg:p-8">
             <p class="text-sm font-medium uppercase tracking-widest text-pink-500">${movie.genre.join(', ')}</p>
-            <p class="text-xl font-bold text-white sm:text-2xl">${movie.title}</p>
+            <p class="text-md font-bold text-white ">${movie.title}</p>
             <div class="mt-10 sm:mt-48 lg:mt-64 flex flex-col justify-center items-center absolute top-0 left-0 w-full h-full opacity-0 transition-opacity group-hover:opacity-100"> <!-- Modificación aquí -->
               <div class="transform transition-transform group-hover:translate-y-0 group-hover:opacity-100"> <!-- Modificación aquí -->
                 <p class="text-sm text-white">
