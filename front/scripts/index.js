13fe37fb85fb32c37a8f999dc7362362fdb1f024
alejo saez <alejosaezgebicki@gmail.com>
2024-03-07 16:31:50 -0300

    actidad 4 divicion de archivo js agragando webpack y mas

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,36 +1,14 @@
-$(document).ready(function() {
-    let container = document.getElementById('movie-container');
-    container.classList.add('d-flex', 'flex-wrap', 'gap-4', 'justify-content-center'); 
+const renderCard = require("./renderCard").default;
 
+// const renderCars = require(renderCars)
+
+
+$(document).ready(function() {
+   
     $.get('https://students-api.2.us-1.fl0.io/movies', function(data) {
-        renderMovieCards(data);
+        renderCard(data);
     }).fail(function() {
         console.error('There was a problem with the request');
     });
 
-    function renderMovieCards(data) {
-        data.forEach(movie => {
-            let article = document.createElement('div');
-            article.className = 'card mb-3 custom-card'; 
-            article.style.width = '280px';
-
-            article.innerHTML = `
-                <img src="${movie.poster}" class="card-img-top" alt="..." style="max-width: 280px;">
-                <div class="card-body">
-                    <p class="card-text text-sm font-medium text-uppercase text-pink-500">${movie.genre.join(', ')}</p>
-                    <h5 class="card-title text-md font-bold text-black">${movie.title}</h5>
-                    <p class="card-text text-sm text-black">
-                        Director: ${movie.director}<br>
-                        Year: ${movie.year}<br>
-                        Duration: ${movie.duration}<br>
-                        Rate: ${movie.rate}
-                    </p>
-                </div>
-            `;
-    
-            container.appendChild(article);
-        });
-
-      
-    }
-});
+})
---
+++ front/scripts/renderCard.js
@@ -0,0 +1,29 @@
+let container = document.getElementById('movie-container');
+container.classList.add('d-flex', 'flex-wrap', 'gap-4', 'justify-content-center'); 
+
+
+function renderCard(data) {
+    data.forEach(movie => {
+        let article = document.createElement('div');
+        article.className = 'card mb-3 custom-card'; 
+        article.style.width = '280px';
+
+        article.innerHTML = `
+            <img src="${movie.poster}" class="card-img-top" alt="..." style="max-width: 280px;">
+            <div class="card-body">
+                <p class="card-text text-sm font-medium text-uppercase text-pink-500">${movie.genre.join(', ')}</p>
+                <h5 class="card-title text-md font-bold text-black">${movie.title}</h5>
+                <p class="card-text text-sm text-black">
+                    Director: ${movie.director}<br>
+                    Year: ${movie.year}<br>
+                    Duration: ${movie.duration}<br>
+                    Rate: ${movie.rate}
+                </p>
+            </div>
+        `;
+
+        container.appendChild(article);})
+    }
+
+    // export default renderCard
+    module.exports = renderCard
