5d9a4f665b77c09e614610ecb0df267f0932d085
alejo saez <alejosaezgebicki@gmail.com>
2024-03-12 13:39:13 -0300

    enrutadores express

---
+++ back/index.js
@@ -0,0 +1,9 @@
+const app= require("./src/server")
+
+app.listen(3001, () => {
+    console.log(`Servidor escuchando en el puerto 3001`);
+  });
+
+
+
+
--- back/src/server.js
+++ back/src/server.js
@@ -1,4 +1,4 @@
-const server= require("express")
+const express= require("express")
 
 const app = express();
 module.exports = app;
