3300a1b84b21648a3466d98e271060cdea4ca0a1
alejo saez <alejosaezgebicki@gmail.com>
2024-03-13 17:44:53 -0300

    extra credit

--- back/src/services/movieService.js
+++ back/src/services/movieService.js
@@ -1,13 +1,30 @@
 const axios = require("axios");
 
+class Movie {
+    constructor({ title, poster, director, year, duration, genre,rate }) {
+        if (!title || !poster || !director) {
+            throw new Error("Se requieren las propiedades 'title', 'poster' y 'director'");
+        }
+
+        this.title = title;
+        this.poster = poster;
+        this.director = director;
+        this.year = year;
+        this.duration = duration;
+        this.genre = genre;
+        this.rate=rate;
+    }
+}
+
 module.exports ={
     getAllMovies : async() =>{
         try {
             const response = await axios.get('https://students-api.up.railway.app/movies');
-            return response.data
+            const moviesData = response.data;
+            const movies = moviesData.map(movie => new Movie(movie));
+            return movies;
         } catch (error) {
-            return ("Error en la solicitud a la API:", error);
-            
+            throw new Error("Error en la solicitud a la API:", error);
         }
     }
 }
